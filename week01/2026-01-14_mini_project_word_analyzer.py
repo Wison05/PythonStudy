84e60d18e8ed67544fa6075a3c5b24ee4588ba57
Wison <alstpfk619@gmail.com>
2026-01-15 15:21:54 +0900

    week01: progress, add mini_project_word_analyzer code

--- week01/2026-01-14_mini_project_word_analyzer.py
+++ week01/2026-01-14_mini_project_word_analyzer.py
@@ -1,3 +1,5 @@
+import re
+
 def read_text_list(L):
     return [word for i in range(L) for word in input().split()]
 
@@ -8,17 +10,53 @@ def make_lower_word(list):
     return [i.lower() for i in list]
 
 def classify_only_alpha(text_list):
-    text_list = [''.join(text for text in list if text.isalpha()) for list in text_list]
-    return [t for t in text_list if t != '']
+    text_list = [''.join(ch for ch in s if ch.isascii() and ch.isalpha()) for s in text_list if not any(ch.isdigit() for ch in s)]
+    return [t for t in text_list if t != '' and len(t)>1]
+
+def reduce_text_length(text_list):
+    return [re.sub(r"(.)\1{2,}",r"\1\1",text) for text in text_list]
+
+def masking_ban_words(text_list,ban_list):
+    return ["***" if text in ban_list else text for text in text_list ]
+
+def ratio_cnt_dict(text_list):
+    text_ratio_dict = {}
+    total = len(text_list)
+    for text in text_list:
+        if text == "***" : continue
+        cnt = text.count(text)
+        ratio = cal_ratio(cnt,total)
+        text_ratio_dict[text] = cnt, ratio
+    return text_ratio_dict
+
+def cal_ratio(cnt,total):
+    return cnt/total
+
+def return_top_n(text_ratio_dict,N):
+    return dict(sorted(text_ratio_dict.items(), key=lambda x: x[1], reverse=True)[:N])
 
 L = int(input()) #review text line number
 text_list = read_text_list(L)
 text_list = make_lower_word(text_list)
 text_list = classify_only_alpha(text_list)
-print(text_list)
+text_list = reduce_text_length(text_list)
 B = int(input()) #banned words number
 ban_words = read_ban_words(B)
-print(ban_words)
+text_list = masking_ban_words(text_list,ban_words)
+text_ratio = ratio_cnt_dict(text_list)
 N = int(input()) #many used word Top N
+text_ratio = return_top_n(text_ratio,N)
+
+print("CLEANED_TEXT:")
+print(' '.join(text_list),"\n")
+print("TOP_WORDS:")
+for key,value in text_ratio.items():
+    print(f"{key}: {value}")
+print()
+print("BAN WORDS:")
+print(' '.join(ban_words))
+
+
+
 
 
